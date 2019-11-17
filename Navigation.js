3e5747517c9d987b5b757495bb229447ffe3a56d
brad <braddoro@gmail.com>
2019-11-16 22:30:49 -0500

    Clean up form widths.

--- Navigation.js
+++ Navigation.js
@@ -2,11 +2,10 @@ isc.defineClass("Navigation", "Menu").addProperties({
 	initWidget: function(initData){
 		this.MainMenu = isc.myMenu.create({
 			title: "Menu",
-			// , width: 200, height: 300
 			items: [
 				{title: "Class", click: "isc.Class.create({title: \"Classes\"})"},
-				{title: "Division", click: "isc.Division.create({title: \"Divisions\", width: 200, height: 300})"},
-				{title: "Group", click: "isc.Group.create({title: \"Groups\", width: 200, height: 300})"},
+				{title: "Division", click: "isc.Division.create({title: \"Divisions\"})"},
+				{title: "Group", click: "isc.Group.create({title: \"Groups\"})"},
 				{isSeparator: true},
 				{title: "Item", click: "isc.Item.create({title: \"Items\", width: \"90%\", height: 300})"}
 			]
